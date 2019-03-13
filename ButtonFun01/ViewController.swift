3494f47795c8864d536ddb8bf6ea99c2add2f163
dit02 <dit@imac-7.local>
2019-03-13 16:17:16 +0900

    commit

--- ButtonFun01/ViewController.swift
+++ ButtonFun01/ViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mylabel: UILabel!
+    @IBOutlet weak var subview: UIView!
+    @IBOutlet weak var but: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor=UIColor.cyan
+        mylabel.text="hellow World !!!!"
+        print(mylabel.text!)
+        subview.backgroundColor=UIColor.red
+        but.backgroundColor=UIColor.yellow
     }
+    var checked=false;
+    @IBAction func but(_ sender: UIButton) {
+        if(checked == false){
+            mylabel.text="Button Pressed 1"
+            checked = true
+            subview.backgroundColor=UIColor.blue
+        }else if(checked == true){
+            mylabel.text="Button Pressed 2"
+            checked = false
+            subview.backgroundColor=UIColor.red
+        }
+    }
+    
 
 
 }
